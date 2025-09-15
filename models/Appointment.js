99af76dd06e7229b858262a21732693f76660848
RafaelCG <rafael.2421971@aluno.faculdadedescomplica.com.br>
2025-09-15 17:41:17 -0300

    Criação do validate em Doctor.js e do validator em doctorId e pacientId em Appointment.js

--- models/Appointment.js
+++ models/Appointment.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import doctor from "./Doctor.js";
+import pacient from "./Pacient.js";
 
 const Schema = mongoose.Schema;
 
@@ -9,11 +11,19 @@ const appointmentSchema = new Schema({
         },
         doctorId: {
             type: String,
-            required: [true, 'DoctorID is required']
+            required: [true, 'DoctorID is required'],
+            validator: async function(v) {
+                return await doctor.exists({ _id: v });
+            },
+            message: props => `DoctorID ${props.value} does not exist`
         },
         pacientId: {
             type: String,
-            required: [true, 'PacientID is required']
+            required: [true, 'PacientID is required'],
+            validator: async function(v){
+                return await pacient.exists({ _id: v });
+            },
+            message: props => `PacientID ${props.value} does not exist`
         },
         createdAt: {
             type: Date,
@@ -24,3 +34,11 @@ const appointmentSchema = new Schema({
 
 const appointment = mongoose.model('Appointment', appointmentSchema);
 export default appointment;
+
+/*se o validator não funcionar, tente essa outra forma de fazer a validação
+    validator: async function(v) {
+                const id = new mongoose.Types.ObjectId(v); //convertendo um string em um objeto ID para ser encontrado no banco
+                return doctor.exists({_id: id}); //verificando se o ID existe no banco
+            },
+           message: props => `DoctorID ${props.value} does not exist`
+*/
