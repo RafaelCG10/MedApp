70d3816248cdaa9001aff62d8806bad663f14674
RafaelCG <rafael.2421971@aluno.faculdadedescomplica.com.br>
2025-09-18 14:37:53 -0300

    Ajustes para upload e leitura de arquivos, modificados arquivos Prescriptions.js, PrescriptionRepository.js, PrescriptionService e PrescripitionController.js

--- models/Prescription.js
+++ models/Prescription.js
@@ -24,6 +24,9 @@ const prescriptionSchema = new Schema({
         createdAt: {
             type: Date,
             default: Date.now
+        },
+        file: {
+            type: String
         }
     }    
 );
